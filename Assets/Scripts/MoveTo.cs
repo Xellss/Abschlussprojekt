23079ac872a5af9a1968923c1b7f0110ad4bf881
Xellss <Xellsnr1@hotmail.de>
2016-04-07 02:13:33 +0200

    Created the Unity Projekt, the first Script, some default textures and behaviors

---
+++ Assets/Scripts/MoveTo.cs
@@ -0,0 +1,21 @@
+﻿/////////////////////////////////////////////////
+///                                           ///
+///      Source Code - Abschlussprojekt       ///
+///                                           ///
+///           Author: Julian Hopp             ///
+///                                           ///
+///                                           ///
+/////////////////////////////////////////////////
+
+using UnityEngine;
+
+public class MoveTo : MonoBehaviour {
+
+    [SerializeField]
+    private Transform goal;
+
+	void Start () {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+}
