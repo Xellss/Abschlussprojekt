825ff7dd699e01b2e0b2ac4dcb9ed3a255b6367a
Daniel Lause <info@daniel-lause.de>
2016-04-14 00:16:18 +0200

    Added building slots to UnlockedTerrain

--- Assets/Scripts/Daniel/BaseTerrainManager.cs
+++ Assets/Scripts/Daniel/BaseTerrainManager.cs
@@ -11,13 +11,14 @@ public class BaseTerrainManager : MonoBehaviour
         set { unlockTerrain = value; }
     }
 
-    private Renderer myRenderer;
     private GameObject buyButton;
-
+    private GameObject towerSlot;
+    Component[] myComponents;
     private void Awake()
     {
-        myRenderer = GetComponent<Renderer>();
+      
         buyButton = transform.FindChild("BuyButton").gameObject;
+        myComponents = transform.GetComponents(typeof(Component));
     }
 
     private void Update()
@@ -28,10 +29,36 @@ public class BaseTerrainManager : MonoBehaviour
 
     private void unlock()
     {
-        this.gameObject.name = "UnlockedTerrain";
-        this.gameObject.tag = "UnlockedTerrain";
-        myRenderer.material.color = Color.green;
+        towerSlot = (GameObject)Resources.Load("Prefabs/TowerSlot");
+        spawnSlots();
+        gameObject.name = "UnlockedTerrain";
+        gameObject.tag = "UnlockedTerrain";
+        clearComponents();
         GameObject.Destroy(buyButton);
-        Component.Destroy(this);
+    }
+
+    private void spawnSlots()
+    {
+        float pointZero = 4.5f;
+        int towerCount = 1;
+
+        for (float x = transform.position.x; x < transform.localScale.x + transform.position.x; x++)
+        {
+            for (float z = transform.position.z; z < transform.localScale.z + transform.position.z; z++)
+            {
+                GameObject newTowerslot = (GameObject)Instantiate(towerSlot, new Vector3(x - pointZero, transform.position.y, z - pointZero), Quaternion.identity);
+                newTowerslot.transform.SetParent(transform);
+                newTowerslot.name = "TowerSlot_" + towerCount;
+                towerCount++;
+            }
+        }
+    }
+    private void clearComponents()
+    {
+        foreach (var component in myComponents)
+        {
+            if (component.GetType() != typeof(Transform))
+                Component.Destroy(component);
+        }
     }
 }
