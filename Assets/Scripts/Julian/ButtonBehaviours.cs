6c3e02ebbce803ea79212fc3c0352a7939dbf3dc
Xellss <Xellsnr1@hotmail.de>
2016-04-15 17:13:30 +0200

    added buttons & button behaviour

---
+++ Assets/Scripts/Julian/ButtonBehaviours.cs
@@ -0,0 +1,72 @@
+﻿/////////////////////////////////////////////////
+///                                           ///
+///      Source Code - Abschlussprojekt       ///
+///                                           ///
+///           Author: Julian Hopp             ///
+///                                           ///
+///                                           ///
+/////////////////////////////////////////////////
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonBehaviours : MonoBehaviour
+{
+    private Transform credits;
+    private Transform options;
+    private Transform startScreen;
+
+    private void Awake()
+    {
+        startScreen = transform.FindChild("StartScreen");
+
+        //credits = startScreen.FindChild("Credit_Image");
+        //credits.gameObject.SetActive(false);
+    }
+
+    // Game Start
+    public void OnClick_GameStart()
+    {
+        startScreen.gameObject.SetActive(false);
+    }
+
+    // Credits
+    public void OnClick_Credits()
+    {
+        credits.gameObject.SetActive(true);
+    }
+
+    public void OnClick_Credits_Back()
+    {
+        credits.gameObject.SetActive(false);
+    }
+
+    // Options
+    public void OnClick_Options()
+    {
+        options.gameObject.SetActive(true);
+    }
+
+    public void OnClick_Options_Back()
+    {
+        options.gameObject.SetActive(false);
+    }
+
+    // Quit
+    public void OnClick_Quit()
+    {
+        Application.Quit();
+    }
+
+    // Tutorial
+    public void OnClick_Tutorial()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Retry
+    public void OnClick_Retry()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
