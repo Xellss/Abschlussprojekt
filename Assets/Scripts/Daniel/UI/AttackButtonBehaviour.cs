30986cb72dd52c6bf902c5c30887140b32cd9f33
Daniel Lause <info@daniel-lause.de>
2016-04-22 12:18:37 +0200

    Add Attack Button with Wave Spawn and MiniGame Button

---
+++ Assets/Scripts/Daniel/UI/AttackButtonBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackButtonBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
