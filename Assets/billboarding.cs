19e985faa1ba211817931dc7cbb68570f99fcea2
GameProg <>
2016-06-06 14:52:39 +0200

    rotationsSpeed + billboarding + play& Pause button

---
+++ Assets/billboarding.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class billboarding : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject MainCamera;
+
+    public void Start()
+    {
+        MainCamera = GameObject.Find("Main Camera");
+    }
+
+    void Update()
+    {
+        transform.LookAt(transform.position + MainCamera.transform.rotation * Vector3.forward,
+          MainCamera.transform.rotation * Vector3.up);
+    }
+}
