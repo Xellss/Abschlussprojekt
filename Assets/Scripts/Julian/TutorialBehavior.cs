f608c44b04b063f7b9c6aa92d8e57ce9ad618653
Xellss <Xellsnr1@hotmail.de>
2016-06-26 20:25:54 +0200

    Tutorial added

---
+++ Assets/Scripts/Julian/TutorialBehavior.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialBehavior : MonoBehaviour
+{
+
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+  public void OnButtonClick()
+    {
+        Time.timeScale = 1;
+        this.gameObject.SetActive(false);
+    }
+}
