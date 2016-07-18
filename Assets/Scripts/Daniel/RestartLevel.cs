c768939eaa1b90bd2b21d81341570f73f95686f1
Daniel Lause <info@daniel-lause.de>
2016-07-18 17:36:19 +0200

    added level restart button/window to Shop

---
+++ Assets/Scripts/Daniel/RestartLevel.cs
@@ -0,0 +1,36 @@
+﻿/////////////////////////////////////////////////
+///                                           ///
+///      Source Code - Abschlussprojekt       ///
+///                                           ///
+///           Author: Daniel Lause            ///
+///                                           ///
+///                                           ///
+/////////////////////////////////////////////////
+using UnityEngine;
+
+public class RestartLevel : MonoBehaviour
+{
+    private WorldSelectionButton button;
+    private WorldMapDetails mapDetails;
+
+    public void OnClickClose()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void OnClickRestart()
+    {
+        mapDetails = GameObject.Find("WorldMapDetails").GetComponent<WorldMapDetails>();
+        button = GameObject.Find("WorldMapDetails").GetComponent<WorldSelectionButton>();
+        mapDetails.CurrentLevelStarCount = 0;
+        button.OnClickWorldButton(mapDetails.CurrentLevel);
+    }
+
+    public void OnClickWorldMap()
+    {
+        mapDetails = GameObject.Find("WorldMapDetails").GetComponent<WorldMapDetails>();
+        button = GameObject.Find("WorldMapDetails").GetComponent<WorldSelectionButton>();
+        mapDetails.CurrentLevelStarCount = 0;
+        button.OnClickWorldMap();
+    }
+}
